47604c04a31f81172114bc3b248b01c981b99109
Jaroslav Langer <langerjara@gmail.com>
2024-09-11 11:04:44 +0200

    fix project

--- generate-data/Program.cs
+++ generate-data/Program.cs
@@ -1,67 +1,107 @@
-﻿using System.Globalization;
+﻿using System;
+using System.IO;
 
-namespace GenerateData;
-
-internal class Program
+class Program
 {
-    static void Main()
+    static Random random = new Random();
+
+    // Funkce pro generování dat
+    static double GenerateTemperature()
     {
-        const int nSamples = 10000; // number of samples to generate
-        var rnd = new Random(42);
+        return 240 + random.NextDouble() * 20;
+    }
 
-        // Data arrays
-        var solderingTemp = new double[nSamples];
-        var placementSpeed = new double[nSamples];
-        var ambientTemp = new double[nSamples];
-        var materialQuality = new double[nSamples];
-        var humidity = new double[nSamples];  
-        var numberOfDefects = new double[nSamples];
-        var cycleTime = new double[nSamples];
+    static double GenerateHumidity()
+    {
+        return 20 + random.NextDouble() * 60;
+    }
 
-        // Generate data
-        for (var i = 0; i < nSamples; i++)
+    static double GenerateMaterialQuality()
+    {
+        return random.NextDouble();
+    }
+
+    static double GenerateAssemblySpeed()
+    {
+        return 30 + random.NextDouble() * 50;
+    }
+
+    static double Sigmoid(double x)
+    {
+        return 1 / (1 + Math.Exp(-x));
+    }
+
+    // Nová upravená funkce pro výpočet pravděpodobnosti vady
+    static double CalculateDefectProbability(double temp, double speed, double quality, double humidity)
+    {
+        return Sigmoid(-0.08 * Math.Pow(speed, 2) - 0.1 * quality + 0.03 * Math.Pow(humidity - 50, 2) + 0.01 * Math.Pow(temp - 250, 2));
+    }
+
+    // Funkce pro určení optimálního rozsahu rychlosti montáže
+    static (double, double) CalculateOptimalSpeedRange(double temp, double quality, double humidity)
+    {
+        double minSpeed = -1;
+        double maxSpeed = -1;
+        bool foundOptimalSpeed = false;
+
+        // Iterujeme přes různé rychlosti montáže a hledáme optimální rozsah
+        for (double speed = 30; speed <= 80; speed += 0.1)
         {
-            solderingTemp[i] = Normal(rnd, 310, 15);  
-            placementSpeed[i] = Normal(rnd, 50, 10);  
-            ambientTemp[i] = Normal(rnd, 22, 5); 
-            materialQuality[i] = Normal(rnd, 0.8, 0.1);  
-            humidity[i] = Normal(rnd, 50, 20);  
-            
-            numberOfDefects[i] = Math.Max(0, 5 - 0.01 * solderingTemp[i] + 0.05 * placementSpeed[i] +
-                                             0.02 * Math.Pow(ambientTemp[i] - 22, 2) - 0.5 * materialQuality[i] +
-                                             0.01 * humidity[i] + Normal(rnd, 0, 0.5));
-            
-            cycleTime[i] = Math.Max(0, 0.4 + 0.002 * solderingTemp[i] + 0.003 * placementSpeed[i] -
-                                       0.001 * ambientTemp[i] + 0.002 * (1 - materialQuality[i]) +
-                                       0.001 * humidity[i] + Normal(rnd, 0, 0.01));
-        }
+            double defectProbability = CalculateDefectProbability(temp, speed, quality, humidity);
 
-        // Write to CSV
-        using (var sw = new StreamWriter("data.csv"))
-        {
-            sw.WriteLine("SolderingTemperature,PlacementSpeed,AmbientTemperature,MaterialQuality,Humidity,NumberOfDefects,CycleTime");
-            for (var i = 0; i < nSamples; i++)
+            if (defectProbability < 0.1)
             {
-                sw.WriteLine($"{solderingTemp[i].ToString(CultureInfo.InvariantCulture)}," +
-                             $"{placementSpeed[i].ToString(CultureInfo.InvariantCulture)}," +
-                             $"{ambientTemp[i].ToString(CultureInfo.InvariantCulture)}," +
-                             $"{materialQuality[i].ToString(CultureInfo.InvariantCulture)}," +
-                             $"{humidity[i].ToString(CultureInfo.InvariantCulture)}," +
-                             $"{numberOfDefects[i].ToString(CultureInfo.InvariantCulture)}," +
-                             $"{cycleTime[i].ToString(CultureInfo.InvariantCulture)}");
+                if (!foundOptimalSpeed)
+                {
+                    minSpeed = speed;  // Nastavíme první nalezenou optimální rychlost
+                    foundOptimalSpeed = true;
+                }
+                maxSpeed = speed;  // Aktualizujeme maximální rychlost
+            }
+            else if (foundOptimalSpeed)
+            {
+                // Pokud byla nalezena optimální rychlost, ale následující už není optimální, smyčku zastavíme
+                break;
             }
         }
 
-        Console.WriteLine("Data generated and saved to data.csv");
+        return (minSpeed, maxSpeed);
     }
 
-    // Function to generate normally distributed random numbers
-    static double Normal(Random rnd, double mean, double stddev)
+    static void GenerateDataset(int numSamples, string filePath, char delimiter = ';', bool skipDefective = false)
     {
-        // Using Box-Muller transform to generate two independent standard normal distributed random numbers
-        var u1 = 1.0 - rnd.NextDouble(); // uniform(0,1] random doubles
-        var u2 = 1.0 - rnd.NextDouble();
-        var randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
-        return mean + stddev * randStdNormal;
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.WriteLine($"Temperature{delimiter}AssemblySpeed{delimiter}MaterialQuality{delimiter}Humidity{delimiter}Defective{delimiter}MinAssemblySpeed{delimiter}MaxAssemblySpeed");
+
+            for (int i = 0; i < numSamples; i++)
+            {
+                double temp = GenerateTemperature();
+                double speed = GenerateAssemblySpeed();
+                double quality = GenerateMaterialQuality();
+                double humidity = GenerateHumidity();
+
+                double defectProbability = CalculateDefectProbability(temp, speed, quality, humidity);
+                int defective = random.NextDouble() < defectProbability ? 1 : 0;
+
+                if (skipDefective && defective == 1)
+                    continue;
+
+                (double minSpeed, double maxSpeed) = CalculateOptimalSpeedRange(temp, quality, humidity);
+
+                // Zápis do CSV
+                writer.WriteLine($"{temp:F2}{delimiter}{speed:F2}{delimiter}{quality:F2}{delimiter}{humidity:F2}{delimiter}{defective}{delimiter}{minSpeed:F2}{delimiter}{maxSpeed:F2}");
+            }
+        }
+
+        Console.WriteLine($"Dataset vygenerován a uložen do souboru {filePath}");
+    }
+
+    static void Main(string[] args)
+    {
+        int numSamples = 10000;
+        string filePath = "dataset_with_new_formula.csv";
+
+        GenerateDataset(numSamples, filePath);
     }
 }
